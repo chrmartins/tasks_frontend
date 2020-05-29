9a96eb499e4c8dd6785f060a8474d1b5e6bf8c6b
chrmartins <cmts2010@gmail.com>
2020-05-28 21:21:29 -0300

    tela de tasks finalizada

---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import { Table, Badge, Button } from 'react-bootstrap';
+
+import moment from 'moment'
+
+import api from '../../services/api'
+
+interface ITask {
+  id: number;
+  title: string;
+  description: string;
+  finished: boolean;
+  create_at: Date;
+  update_at: Date;
+}
+
+const Tasks: React.FC = () => {
+
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  useEffect(() => {
+    loadTasks()
+  }, [])
+
+  async function loadTasks() {
+    const response = await api.get('/tasks')
+    console.log(response)
+    setTasks(response.data)
+  }
+
+  function formatDate(date: Date) {
+    return moment(date).format("DD/MM/YYYY")
+  }
+
+  return(
+    <>
+      <div className="container">
+        <br/>
+        <h1>Tasks Page</h1>
+        <br/>
+        <Table striped bordered hover className="text-center">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Título</th>
+              <th>Data de Atualização</th>
+              <th>Status</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              tasks.map(task => (
+                <tr key={task.id}>
+                  <td>{task.id}</td>
+                  <td>{task.title}</td>
+                  <td>{ formatDate(task.update_at) }</td>
+                  <td>
+                    <Badge variant={ task.finished ? "success" : "warning" }>
+                      {task.finished ? "Finalizado" : "Pendente"}
+                    </Badge>
+                  </td>
+                  <td>
+                    <Button size="sm">Editar</Button>{' '}
+                    <Button size="sm" variant="success">Finalizar</Button>{' '}
+                    <Button size="sm" variant="info">Visualizar</Button>{' '}
+                    <Button size="sm" variant="danger">Remover</Button>
+                  </td>
+                </tr>                  
+              ))
+            }
+
+          </tbody>
+        </Table>
+      </div>
+    </>
+  ) 
+}
+
+export default Tasks;
